ef950034f77081af7438dc2f470bd4afe96f8ab7
demesameneshoa <demesameneshoa@gmail.com>
2024-01-09 00:26:04 +0300

    Create Models and Migrations  - create category model and db migration  - create expense model and db migration  - create categoryexpense model and db migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :expenses
+  resources :categories
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "categories#index"
+  root "categories#index"
 end
